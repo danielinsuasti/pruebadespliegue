88ca3ee2e83eefcb7a8e0a10e6dc9bd94e489a44
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-12-03 14:56:45 -0500

    Modificar codigo para despliegue

---
+++ server.js
@@ -0,0 +1,51 @@
+// hacer el import de express tradicional
+// const express = require('express');
+
+// hacer el nuevo import
+import Express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+import rutasVehiculo from './views/vehiculos/rutas.js';
+import rutasUsuario from './views/usuarios/rutas.js';
+import rutasVenta from './views/ventas/rutas.js';
+import autorizacionEstadoUsuario from './middleware/autorizacionEstadoUsuario.js';
+
+dotenv.config({ path: './.env' });
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://misiontic-concesionario-motos.us.auth0.com/.well-known/jwks.json', //MODIFICADO
+  }),
+  audience: 'api-autenticacion-concesionario-motos',   //MODIFICADO
+  issuer: 'https://misiontic-concesionario-motos.us.auth0.com/', //MODIFICADO
+  algorithms: ['RS256'],
+});
+
+// 4 y 5: enviarle el token a auth0 para que devuelva si es valido o no
+app.use(jwtCheck);
+
+// app.use(autorizacionEstadoUsuario);
+
+app.use(rutasVehiculo);
+app.use(rutasUsuario);
+app.use(rutasVenta);
+
+const main = () => {
+  return app.listen(process.env.PORT, () => {
+    console.log(`escuchando puerto ${process.env.PORT}`);
+  });
+};
+
+conectarBD(main);
